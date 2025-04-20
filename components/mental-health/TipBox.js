5a78a10071ddf2b65a31488e78abddbe44d54cb9
imayankss <135892708+imayankss@users.noreply.github.com>
2025-04-20 12:40:49 +0530

    Delete components directory

--- components/mental-health/TipBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TipBox = ({text}) => {
-  return (
-    <div className='border-[1px] border-gray-200 p-2 flex items-center font-productSansBold rounded-xl'>{text}</div>
-  )
-}
-
-export default TipBox
